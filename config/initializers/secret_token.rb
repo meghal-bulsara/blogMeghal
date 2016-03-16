3e2042fe92fc8786506b99207a34666da66f83b1
meghal <meghalbulsara22@gmail.com>
2016-03-16 12:14:21 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogMeghal::Application.config.secret_key_base = '9770ab6d34744d6b14d99cf5d8373aa0e351fafb36aaa81cc1b1dfe8fdbb46dcc7b8724b588429d47ea5328cd5f7a61ef1de452ee30d226a0b1111d2a9fbfeae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlogMeghal::Application.config.session_store :cookie_store, key: '_blogMeghal_session'
